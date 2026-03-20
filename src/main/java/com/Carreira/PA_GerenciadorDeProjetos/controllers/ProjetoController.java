3b614d2af17d912ca4a422ca919f61cb5d184eba
MiguelCarreiraa <miguelitocarreira@hotmail.com>
2026-03-20 11:56:11 -0300

    Controller

---
+++ src/main/java/com/Carreira/PA_GerenciadorDeProjetos/controllers/ProjetoController.java
@@ -0,0 +1,45 @@
+package com.Carreira.PA_GerenciadorDeProjetos.controllers;
+
+
+import com.Carreira.PA_GerenciadorDeProjetos.models.ProjetoModel;
+import com.Carreira.PA_GerenciadorDeProjetos.services.ProjetoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path="/projetos")
+public class ProjetoController {
+
+    @Autowired
+    private ProjetoService projetoService;
+
+    @GetMapping
+    public List<ProjetoModel> findAll(){
+        return projetoService.findAll();
+    }
+
+    @PostMapping
+    public ProjetoModel criarProjeto(@RequestBody ProjetoModel projetoModel){
+        return projetoService.criar(projetoModel);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<ProjetoModel> bucarProjeto(@PathVariable Long id){
+        return projetoService.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public ProjetoModel atualizarCProjeto(@PathVariable Long id, @RequestBody ProjetoModel projetoModel){
+        return projetoService.atualizar(id, projetoModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarProjeto(@PathVariable Long id){
+        projetoService.deletar(id);
+    }
+
+}
+
